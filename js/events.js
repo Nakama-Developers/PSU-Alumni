86d7f0ede9917718a4336f4b699258862929dc6f
Ala'a Ezziddin <alaa.ezziddin.aae@gmail.com>
2016-12-23 14:46:03 +0300

    - Bug Fixes.

--- js/events.js
+++ js/events.js
@@ -15,22 +15,31 @@ $(document).ready(function () {
 
     // note popup
     var modal = document.getElementById('myModal');
+    var note = document.getElementsByClassName('note-opened');
+
     $('.write-note').click(function () {
-        $('.modal').css('display', 'block');
+        //$('.modal').css('display', 'block');
+        $('.note-container').addClass('note-opened');
+        $('.write-note').addClass('opened-icon');
     })
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close-popup")[0];
+    var closeNote = document.getElementsByClassName("close-note")[0];
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     }
+
+    $('.close-note').click(function () {
+        $('.note-container').removeClass('note-opened');
+        $('.write-note').removeClass('opened-icon');
+    })
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target == note) {
+            $('.note-container').removeClass('note-opened');
         }
     }
 
-})
-
+});
